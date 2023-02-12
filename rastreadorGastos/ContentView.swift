82aadd1f29c1d6b347eabc79653bfed1279ff5dd
Gustavo <tavoludra@gmail.com>
2023-02-12 17:11:43 -0500

    Initial Commit

---
+++ rastreadorGastos/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  rastreadorGastos
+//
+//  Created by GAPT on 12/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hola Mundo!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ rastreadorGastos/rastreadorGastosApp.swift
@@ -0,0 +1,17 @@
+//
+//  rastreadorGastosApp.swift
+//  rastreadorGastos
+//
+//  Created by GAPT on 12/02/23.
+//
+
+import SwiftUI
+
+@main
+struct rastreadorGastosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
